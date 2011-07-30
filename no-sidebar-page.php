92142dcf435349e1840544ce99e0fb3546951398
Alex Chapin <achapin@gmail.com>
2011-07-30 15:40:11 -0400

    added page template with no sidebar

---
+++ no-sidebar-page.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Template Name: No Sidebar Template
+ * Description: A Page Template that adds a sidebar to pages
+ *
+ * @package WordPress
+ * @subpackage Twenty_Eleven
+ * @since Twenty Eleven 1.0
+ */
+
+get_header(); ?>
+
+		<div id="primary">
+			<div id="content" role="main">
+
+				<?php the_post(); ?>
+
+				<?php get_template_part( 'content', 'page' ); ?>
+
+				<?php comments_template( '', true ); ?>
+
+			</div><!-- #content -->
+		</div><!-- #primary -->
+
+<?php get_footer(); ?>
