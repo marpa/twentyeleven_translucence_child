37e781c5fd0e3e54ab8c7a02824ecb7b028b4525
Alex Chapin <achapin@gmail.com>
2011-07-31 16:50:08 -0400

    removed twentyeleven_posted_on function

--- functions.php
+++ functions.php
@@ -170,24 +170,6 @@ function twentyeleven_comment( $comment, $args, $depth ) {
 	endswitch;
 }
 
-/**
- * Prints HTML with meta information for the current post-date/time and author.
- * Create your own twentyeleven_posted_on to override in a child theme
- *
- * @since Twenty Eleven 1.0
- */
-function twentyeleven_posted_on() {
-	printf( __( '<span class="sep">Posted on </span><a href="%1$s" title="%2$s" rel="bookmark"><time class="entry-date" datetime="%3$s" pubdate>%4$s</time></a><span class="by-author"> <span class="sep"> by </span> <span class="author vcard"><a class="url fn n" href="%5$s" title="%6$s" rel="author">%7$s</a></span></span>', 'twentyeleven' ),
-		esc_url( get_permalink() ),
-		esc_attr( get_the_time() ),
-		esc_attr( get_the_date( 'c' ) ),
-		esc_html( get_the_date() ),
-		esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
-		sprintf( esc_attr__( 'View all posts by %s', 'twentyeleven' ), get_the_author() ),
-		esc_html( get_the_author() )
-	);
-}
-
 add_filter( 'twentyeleven_color_schemes', 'twentyeleven_color_schemes_blue' );
 add_action( 'twentyeleven_enqueue_color_scheme', 'twentyeleven_enqueue_color_scheme_blue' );
 
